09a1fb91485c5d4e99e9d072ddaee43dd9aaab6f
Divyansh-saxena <64483506+Divyansh-saxena@users.noreply.github.com>
2020-06-13 19:46:31 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,6 +1,6 @@
 Welcome to My Site
 
-<img src=' d1zxt3kulsqtr0.cloudfront.net/upload.jpg'  width='500' height='500' />
+<img src='https://divyanshbu22cketaws.s3.ap-south-1.amazonaws.com/upload.jpg'  width='500' height='500' />
 <br />
 
 <pre>
