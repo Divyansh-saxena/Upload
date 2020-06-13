a106acf8f3e53ae3359c2aa43b4158331aaafaf6
Divyansh-saxena <64483506+Divyansh-saxena@users.noreply.github.com>
2020-06-13 21:31:56 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,6 +1,5 @@
 Welcome to My Site
 
-<img src='https://divyanshbu22cketaws.s3.ap-south-1.amazonaws.com/upload.jpg'  width='500' height='500' />
 <br />
 
 <pre>
@@ -10,3 +9,5 @@ print `ifconfig`;
 
 ?>
 <?pre>
+
+<img src='https://divyanshbu22cketaws.s3.ap-south-1.amazonaws.com/upload.jpg'  width='500' height='500' />
